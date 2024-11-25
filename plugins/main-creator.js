a7aeb3530904f628c17e46e6b68664060c3133c9
AdaAbigail <girl.dream.sans01@gmail.com>
2024-11-25 15:22:39 -0300

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;ㅤㅤㅤㅤㅤ…⁠ᘛDaniel;;\nFN:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nORG:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nTITLE:\nitem1.TEL;waid=51913776697:51913776697\nitem1.X-ABLabel:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;ㅤㅤㅤㅤㅤ…⁠ᘛDaniel;;\nFN:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nORG:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nTITLE:\nitem1.TEL;waid=543813355709:543813355709\nitem1.X-ABLabel:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '⌯ ׅ🐝ׁ ˳ ʿ α︪︩ввуу 𓂃𔘓', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
