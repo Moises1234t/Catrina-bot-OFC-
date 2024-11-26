b1a31f4e306dce2bfda705af606a9f7864716010
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-11-25 20:24:07 -0500

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;ㅤㅤㅤㅤㅤ…⁠ᘛDaniel;;\nFN:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nORG:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nTITLE:\nitem1.TEL;waid=51913776697:5493813355709\nitem1.X-ABLabel:⌯ ׅ🐝ׁ ˳ ʿ α︪︩ввуу 𓂃𔘓\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:ㅤㅤㅤㅤㅤ…⁠ᘛDaniel⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: '⌯ ׅ🐝ׁ ˳ ʿ α︪︩ввуу 𓂃𔘓', contacts: [{ vcard }] }}, {quoted: m})
+conn.sendContact(m.chat, [['5493813355709', 'abby'], ['51913776697', 'Haru']], fkontak)
 }
 handler.help = ['owner']
 handler.tags = ['main']
